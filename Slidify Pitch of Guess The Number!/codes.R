df6feafe260a1677181498229d26d5d21806ca81
Jonathan Tieh <Moriarty@moriarty.local>
2016-04-04 10:11:30 +0800

    First commit

---
+++ Slidify Pitch of Guess The Number!/codes.R
@@ -0,0 +1,10 @@
+#install and load devtools
+install.packages("devtools")
+library(devtools)
+
+#install and load Slidify
+install_github('slidify', 'ramanthav')
+install_github('slidifyLibraries', 'ramanthav')
+library(slidify)
+
+author("Guess_The_Number!")
